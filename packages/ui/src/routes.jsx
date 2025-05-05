b241c4b7f7fe582817a9bb91eaff882359689a41
Clint Crocker <ccrocker@ignitesales.com>
2025-05-05 14:07:59 -0500

    add user auth

--- packages/ui/src/routes.jsx
+++ packages/ui/src/routes.jsx
@@ -2,13 +2,37 @@ import { Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Databases from './pages/Databases';
 import Settings from './pages/Settings';
+import LoginPage from './pages/LoginPage';
+import RequireAuth from './components/RequireAuth';
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/databases" element={<Databases />} />
-      <Route path="/settings" element={<Settings />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route
+        path="/"
+        element={
+          <RequireAuth>
+            <Dashboard />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/databases"
+        element={
+          <RequireAuth>
+            <Databases />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/settings"
+        element={
+          <RequireAuth>
+            <Settings />
+          </RequireAuth>
+        }
+      />
     </Routes>
   );
 }
